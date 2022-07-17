b69d978348530b6c1a33bae45e7b5ecb2a42f91b
GitHub Action <action@github.com>
2022-07-17 02:17:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/tkocactus/enval/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tkocactus/enval/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tkocactus.enval.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tkocactus.enval.MyBundle
 
 class MyApplicationService {
 
